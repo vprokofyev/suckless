b5f316b6190c2d221519313af49803a582520eae
vprokofyev <v.prokofyev@gmail.com>
2023-02-27 20:11:14 +0300

    stterm: font increased

--- stterm-0.9/config.h
+++ stterm-0.9/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode NFM:style=Retina:pixelsize=13:antialias=true:autohint=true";
+static char *font = "FiraCode NFM:style=Retina:pixelsize=14:antialias=true:autohint=false";
 static int borderpx = 2;
 
 /*
