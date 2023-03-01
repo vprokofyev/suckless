eac524186cd4d2a526ebd04c41d6a7b90daab150
vprokofyev <v.prokofyev@gmail.com>
2023-03-01 13:57:34 +0300

    slstatus deleted, minor font changes, stterm: fontsize patch with -z added

--- stterm-0.9/config.h
+++ stterm-0.9/config.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode NFM:style=Retina:pixelsize=14:antialias=true:autohint=false";
+/*static char *font = "FiraCode NFM:style=Retina:pixelsize=14:antialias=true:autohint=false";*/
+static char *font = "Consolas NF:style=Book:pixelsize=14:antialias=true:autohint=false";
 static int borderpx = 2;
 
 /*
