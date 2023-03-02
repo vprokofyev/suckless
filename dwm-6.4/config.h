5bf99b04021156675d2305ee52c8fad4b2468b11
vprokofyev <v.prokofyev@gmail.com>
2023-03-02 15:44:54 +0300

    dwm: scroll through tabs with shiftviewclients patch added

--- dwm-6.4/config.h
+++ dwm-6.4/config.h
@@ -76,10 +76,12 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_k,      focusstackhid,  {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	/*{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },*/
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY,                       XK_Right,       shiftviewclients, { .i = +1 } },
+	{ MODKEY,                       XK_Left,        shiftviewclients, { .i = -1 } },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
