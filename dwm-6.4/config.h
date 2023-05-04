55ff8f0cbc0657179643587f4656d1d5db16bf0c
vprokofyev <v.prokofyev@gmail.com>
2023-05-04 10:59:17 +0300

    dwm: Modkey+tab switch beetween tags

--- dwm-6.4/config.h
+++ dwm-6.4/config.h
@@ -91,7 +91,7 @@ static const Key keys[] = {
         //{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
         //{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
-	//{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY,                       XK_Tab,    view,           {0} },
         { MODKEY,                       XK_q,    view,           {0} },
 	{ MODKEY,                       XK_Right,       shiftviewclients, { .i = +1 } },
 	{ MODKEY,                       XK_Left,        shiftviewclients, { .i = -1 } },
